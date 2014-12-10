dfa9484be1c5e46e7cd687f58e96f1923e137f6c
Niklas Christoffer Petersen <niklascp@gmail.com>
2014-12-10 20:36:04 +0100

    Migrating to JSON for Settings Storage.

---
+++ src/nCode/Data/CamelCaseContractResolver.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nCode.Data
+{
+    class CamelCaseContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var res = base.CreateProperty(member, memberSerialization);
+
+            var attrs = member.GetCustomAttributes(typeof(JsonPropertyAttribute), true);
+
+            if (attrs.Any())
+            {
+                var attr = (attrs[0] as JsonPropertyAttribute);
+                if (res.PropertyName != null && attr.PropertyName != null)
+                    res.PropertyName = attr.PropertyName;
+            }
+
+            return res;
+        }
+    }
+}
