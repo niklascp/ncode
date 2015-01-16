edcc34e221996773ecf7a599e06ea64116493ca1
Niklas Christoffer Petersen <niklascp@gmail.com>
2015-01-16 18:54:37 +0100

    Update Search Framework API

---
+++ src/nCode/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+using nCode.JobScheduling;
+using nCode.Search;
+
+namespace nCode.Controllers
+{
+    /// <summary>
+    /// Controller API for Job Scheduling.
+    /// </summary>
+    [RoutePrefix("admin/system/search")]
+    public class SearchController : ApiController
+    {
+        /// <summary>
+        /// Gets a list of Planned and Completed Job Executions.
+        /// </summary>
+        [Route("reindex/{sourceGuid}")]
+        [HttpPost]
+        public bool Reindex(Guid sourceGuid)
+        {
+            if (!JobHandler.IsInitialized)
+                return false;
+
+            JobHandler.Engine.Queue<ReindexSourceJob, ReindexSourceJobParameters>(new ReindexSourceJobParameters
+            {
+                SourceGuid = sourceGuid
+            });
+
+            return true;
+        }
+
+
+    }
+}
